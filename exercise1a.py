712b888857e45f5df23387224b10476b8276c852
Steve Liu <liu.yuzaiusa@gmail.com>
2025-08-21 13:23:40 -0400

    Add more explanation to exercise1a

--- exercise1a.py
+++ exercise1a.py
@@ -3,7 +3,11 @@ In this exercise, we will use three functions from the helper_functions module:
     1. start_play: Start a playback stream with a given sample rate. You will always need a stream to play a sound.
     2. play_frequency: Play a single frequency sound for a specified duration.
     3. stop_play: Stop the playback stream. It is a good practice to stop the stream after you finish playing sounds.
-Think about how to use these functions in this code.
+Think about how to make these functions available in this code (hint: by import).
+
+You need to initialize a player (stream) before you play any sound by running start_play() to get a stream object.
+Then, you are free to play any sound you like to on that stream object.
+By the end, you need to detroy the player (stream) by stop_play(stream).
 
 The goal is to play the first measure of Twinkle Twinkle Little Star, which consists of the following notes:
 - C4, C4, G4, G4, A4, A4, G4
