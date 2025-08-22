9a757a419efdcc3bbce52f78158383d5d8f7f684
Steve Liu <liu.yuzaiusa@gmail.com>
2025-08-21 20:30:43 -0400

    Update exercise2b instruction

--- exercise2b.py
+++ exercise2b.py
@@ -26,12 +26,11 @@ import pyaudio
 from typing import Dict, List, Optional, Union
 from helper_functions import start_play, stop_play
 
-def play_score_file(stream: pyaudio.Stream, filename: str, tempo: int):
+def play_score_file(stream: pyaudio.Stream, filename: str):
     """
     Play a score from a file given a set of parameters
     :param stream: PyAudio stream object
     :param filename: Name of the file containing the score string in the format "<note1>/<length1>,<note2>/<length2>, ..., <noteN>/<lengthN>"
-    :param tempo: Tempo in beats per minute (BPM) for a quarter note
     :return: None
     """
     pass  # TODO: implement this function
