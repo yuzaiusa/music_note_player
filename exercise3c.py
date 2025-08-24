49504398bf4d4e39242d8ff6a7444635adf17a56
Steve Liu <liu.yuzaiusa@gmail.com>
2025-08-24 18:32:43 -0400

    Updated exercise 3b and 3c

---
+++ exercise3c.py
@@ -0,0 +1,14 @@
+"""
+(Optional) Exercise 3c: Music Note Player with Keyboard Input
+
+Based on the class MusicNotePlayer from exercise3a.py (or 3b), you are tasked to implement a class
+derived from it with additional functionality to take input from keyboard strokes to record and play notes.
+
+You will need to install pynput to handle keyboard input. To install it, run:
+    pip install pynput
+
+TODO: explain pynput usage here
+"""
+
+if __name__ == "__main__":
+    pass  # TODO: implement here
